642aef025990a32f405e056ad0826ddc56c6cdaf
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-07-27 00:29:41 -0700

    update airule.metadata

--- airule/__init__.py
+++ airule/__init__.py
@@ -2,7 +2,7 @@ import json
 from types import SimpleNamespace
 
 
-metadata = SimpleNamespace(**json.load('metadata.json'))
+metadata = SimpleNamespace(**json.load(open('metadata.json')))
 
 
 __version__ = metadata.VERSION
