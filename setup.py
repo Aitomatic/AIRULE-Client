42d25cad531abc1a856e766f52dddd2d9a0ea11f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-07-26 23:26:43 -0700

    add airule.cli:airule & update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 from setuptools import find_packages, setup
+from types import SimpleNamespace
 
 
 AIRULE_CLIENT_PACKAGE_NAME = 'airule'
@@ -8,35 +9,34 @@ AIRULE_CLIENT_PACKAGE_NAME = 'airule'
 
 current_dir_path = Path(__file__).parent
 
-metadata = json.load(open(current_dir_path /
-                          AIRULE_CLIENT_PACKAGE_NAME /
-                          'metadata.json'))
-
-with open(current_dir_path / 'README.md', encoding='utf8') as f:
-    long_description = f.read()
-
-with open(current_dir_path / 'requirements.txt') as f:
-    requirements = f.readlines()
+metadata = SimpleNamespace(**json.load(open(current_dir_path /
+                                       AIRULE_CLIENT_PACKAGE_NAME /
+                                       'metadata.json')))
 
 
 setup(
-    name=metadata['PACKAGE'],
-    version=metadata['VERSION'],
-    description=metadata['DESCRIPTION'],
-    long_description=long_description,
+    name=metadata.PACKAGE,
+    version=metadata.VERSION,
+    description=metadata.DESCRIPTION,
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author=metadata['AUTHOR'],
-    url=metadata['URL'],
-    download_url=metadata['DOWNLOAD_URL'],
+    author=metadata.AUTHOR,
+    maintainer=metadata.AUTHOR,
+    url=metadata.URL,
+    download_url=metadata.DOWNLOAD_URL,
     packages=find_packages(include=[f'{AIRULE_CLIENT_PACKAGE_NAME}.*']),
+    py_modules=[],
     scripts=[],
-    classifiers=metadata['CLASSIFIERS'],
-    license='Apache 2.0',
-    keywords=metadata['KEYWORDS'],
+    classifiers=metadata.CLASSIFIERS,
+    license='MIT',
+    keywords=metadata.KEYWORDS,
+    package_dir={'': ''},
     include_package_data=True,
     zip_safe=False,
-    install_requires=requirements,
-    entry_points='',
+    install_requires=open('src/requirements.txt').readlines(),
+    entry_points=dict(console_scripts=['airule=airule.cli:airule']),
+    extras_require={},
     python_requires='>= 3.8',
+    setup_requires=[],
     namespace_packages=[]
 )
