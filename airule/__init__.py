6773ea9e2b4f8638a4c4bfd32c82b0c77b086ba9
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-08-11 21:57:08 -0700

    add skeletal airule.data & .model modules

--- airule/__init__.py
+++ airule/__init__.py
@@ -2,9 +2,18 @@ import json
 from pathlib import Path
 from types import SimpleNamespace
 
+from .data import DataSet
+from .model import Model
+
+
+__all__ = [
+    '__version__',
+    'DataSet',
+    'Model'
+]
+
 
 metadata = SimpleNamespace(**json.load(open(Path(__file__).parent /
                                             'metadata.json')))
-
 
 __version__ = metadata.VERSION
