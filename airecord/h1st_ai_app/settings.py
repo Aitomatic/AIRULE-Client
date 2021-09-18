42d93fd7b99d63f8f51b4c488a470ebc55eafd76
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-09-17 22:03:21 -0700

    minor edit to airecord.h1st_ai_app.settings

--- airecord/h1st_ai_app/settings.py
+++ airecord/h1st_ai_app/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/dev/topics/i18n/
+# https://docs.djangoproject.com/en/dev/topics/i18n
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
