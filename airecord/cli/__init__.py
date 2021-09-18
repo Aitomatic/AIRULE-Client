dcfec5ecb2a57b102dc5b65c95932755f8a682a0
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-09-17 23:32:23 -0700

    update airecord.cli.airecord command

--- airecord/cli/__init__.py
+++ airecord/cli/__init__.py
@@ -1,6 +1,16 @@
+import sys
+
 import click
+
+from django.core.management import call_command
+
+from ..h1st_ai_app import __path__
 
 
 @click.command()
 def airecord():
-    pass
+    # make sure Django picks up the settings.py in airecord/h1st_ai_app
+    sys.path.insert(0, __path__[0])
+
+    # run Django server locally
+    call_command('runserver')
